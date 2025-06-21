55bf14df52f19b0271c8539616d6d3bb60440d8b
goodispotato <potatoisgood@ewhain.net>
2025-06-21 22:52:12 +0900

    week 14

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Accommodate />
   </React.StrictMode>
 );
 
